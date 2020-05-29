ab2ed308fe7bdd99d96766247afdfa4a7d2a83ec
victoria gonzalez <gonzalezevic@gmail.com>
2020-05-29 04:00:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, Neil deGrasse Tyson !" + specialGuest + "!";
 
-var topic = "space";
+var topic = "Lets talk about space.";
 
 var conversation = `${topic}`;
